007e3ade5c2d2ac80244c9b0827f20ac853bb699
lorenzorru0 <lorenzoorru999@gmail.com>
2021-11-05 15:47:41 +0100

    first commit

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DC Comics</title>
+    <link rel="stylesheet" href="{{assets('css/app.css')}}">
+</head>
+<body>
+    
+    @include('partials.header')
+
+    <main>
+        @yield('mainContent')
+    </main>
+
+    @include('partials.footer')
+
+</body>
+</html>

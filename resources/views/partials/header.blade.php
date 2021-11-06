c92e28feb648b73c14e9744b59d7f68bf2ddc03f
lorenzorru0 <lorenzoorru999@gmail.com>
2021-11-06 09:23:20 +0100

    Fix header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,33 @@
 <header>
     <div class="headerTop">
-        <div>DC POWER &#8480; VISA &#174;</div>
-        <div>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></div>
+        <div class="container">
+            <div>DC POWER &#8480; VISA &#174;</div>
+            <div>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></div>
+        </div>
     </div>
-    <div class="headerBottom">
-        <div>
-            <img src="{{asset('images/dc-logo.png')}}" alt="Logo DC Comics">
-        </div>
-        <div>
-            <ul>
-                @foreach ($link as $navLink)
-                    @if ($navLink['active'] == false)
-                        <li><a href="{{$navLink['link']}}">{{$navLink['name']}}</a></li>
-                    @else
-                        <li><a href="{{$navLink['link']}}" class="active">{{$navLink['name']}}</a></li>
-                    @endif
-                @endforeach
-            </ul>
-        </div>
-        <div>
-            <input type="text" name="search" id="search">
+    <div class="container">
+        <div class="headerBottom">
+            
+            <div>
+                <img src="{{asset('images/dc-logo.png')}}" alt="Logo DC Comics">
+            </div>
+            <div>
+                <ul>
+                    @foreach ($link as $navLink)
+                        @if ($navLink['active'] == false)
+                            <li><a href="{{$navLink['link']}}">{{$navLink['name']}}</a></li>
+                        @else
+                            <li><a href="{{$navLink['link']}}" class="active">{{$navLink['name']}}</a></li>
+                        @endif
+                    @endforeach
+                </ul>
+            </div>
+            <div>
+                <div class="containerSearch">
+                    <input type="text" name="search" id="search" placeholder="Search">
+                    <i class="fas fa-search"></i>
+                </div>
+            </div>
         </div>
     </div>
 </header>

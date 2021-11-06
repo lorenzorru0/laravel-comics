889c50e56793f1756553ff9914283f82607797c8
lorenzorru0 <lorenzoorru999@gmail.com>
2021-11-06 09:47:08 +0100

    Add comics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layout.base')
 
 @section('mainContent')
-    
+    <section class="containerSeries">
+        <div class="container">
+            <div>
+                <h2>CURRENT SERIES</h2>
+            </div>
+
+            <div class="seriesContainer">
+                @foreach ($comics as $comic)
+                    <div class="series">
+                        <img src="{{$comic['thumb']}}" :alt="{{$comic['title']}}">
+                        <p>{{$comic['title']}}</p>
+                    </div>
+                @endforeach
+            </div>
+
+            <div>
+                <button>LOAD MORE</button>
+            </div>
+        </div>
+    </section>
 @endsection
